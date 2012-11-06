8fbc0fe2d0e0d00f4a80ba20aa53bd02e0ae2ffb
Sven Nilsen <sven@cutoutpro.com>
2012-11-06 14:48:51 +0100

    Changed testing to 1 million items.

--- test-ugroup.c
+++ test-ugroup.c
@@ -125,18 +125,13 @@ int main(int argc, char *argv[])
 	test_3();
 	
 	int i;
-	int end = 1 << 25;
+	int end = 1 << 10;
 	
-	int_group *a = int_group_Array(30, (int[]){
-		  0,   2,   4,   6,   8,  10,  12,  14,  16,  18,
-		 20,  22,  24,  26,  28,  30,  32,  34,  36,  38,
-		 40,  42,  44,  46,  48,  50,  52,  54,  56,  58
-	});
-	int_group *b = int_group_Array(30, (int[]){
-		  1,   3,   5,   7,   9,  11,  13,  15,  17,  19,
-		 21,  23,  25,  27,  29,  31,  33,  35,  37,  39,
-		 41,  43,  45,  47,  49,  51,  53,  55,  57,  59
-	});
+	int lim = 1000000;
+	int arr[lim];
+	for (i = 0; i < lim; i++) arr[i] = i;
+	int_group *a = int_group_Array(lim, arr);
+	int_group *b = int_group_Array(lim, arr);
 	
 	for (i = 0; i < end; i++) {
 		int_group *c = int_group_And(a, b);
